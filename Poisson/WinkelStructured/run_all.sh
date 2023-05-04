34f2fc6e38d52cbb502d289136f687e2b0b9a7e5
krantamaki <kasper.rantamaki@csc.fi>
2023-05-04 15:01:48 +0300

    Added specific cases for data visualization

--- Poisson/WinkelStructured/run_all.sh
+++ Poisson/WinkelStructured/run_all.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+# Remove the result files if they already exist
+rm -f results/f.*
+
+# Define the mesh level to be used
+mesh_level=3
+
 for s in linsys/*.sif; do
+
+    echo ""
+    echo ""
     echo '----------------------------------------------'
     echo 'starting: '$s  
     cp $s linsys.sif
-    ElmerSolver
+    ElmerSolver case_all.sif -ipar 1 $mesh_level
     echo 'ending: '$s  
     echo '----------------------------------------------'
+    echo ""
+    echo ""
+    
 done
 
