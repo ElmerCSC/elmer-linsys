ce575d91ada14b9530b59bdae855eabb8d3026a6
krantamaki <kasper.rantamaki@csc.fi>
2023-05-04 17:00:26 +0300

    Scalability visualization changed to loglog-scale

--- Poisson/python-scripts/plot_scalability.py
+++ Poisson/python-scripts/plot_scalability.py
@@ -1,5 +1,5 @@
 """
-Basic Python script that plots the execution times of a given linear solver as a function of the Mesh Level and fits an exponential to it
+Basic Python script that plots the execution times of a given linear solver as a function of the number of elements and fits an exponential to it
 """
 import numpy as np
 import pandas as pd
@@ -36,19 +36,19 @@ def main():
     data = data[data['norm'] != 0.0]
 
     times = data[use_time].values.tolist()
-    mesh_levels = data['MeshLevel'].values.tolist()
+    elements = data['elements'].values.tolist()
 
     # Fit an exponential to the given datapoints
-    popt, pcov = curve_fit(lambda t, a, b, c: a * np.exp(b * t) + c, mesh_levels, times)
-    x = np.linspace(np.min(mesh_levels), np.max(mesh_levels), 100)
-    y = popt[0] * np.exp(popt[1] * x) + popt[2]
+    popt, pcov = curve_fit(lambda n, a, b: a * n ** b, elements, times)
+    x = np.linspace(np.min(elements), np.max(elements), 100)
+    y = popt[0] * x ** popt[1]
 
     # Plot the datapoints and fitted exponential
     plt.figure()
-    plt.plot(x, y, 'k', label=f"Found exponential: {popt[0]:.2e}*exp({popt[1]:.2e}x) + {popt[2]:.2e}")
-    plt.plot(mesh_levels, times, 'r.', label="Datapoints")
+    plt.loglog(x, y, 'k', label=f"Found exponential: $({popt[0]:.2e}) \cdot n ^ {{{popt[1]:.2e}}}$")
+    plt.loglog(elements, times, 'r.', label="Datapoints")
     plt.ylabel(use_time)
-    plt.xlabel("Mesh Level")
+    plt.xlabel("#Elements = $n$")
     plt.title(f"Solver: {data['Solver'][0]}")  # Assumes that the file contains results for only one solver
     plt.legend()
     plt.show()
