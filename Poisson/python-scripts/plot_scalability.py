c70825d1fa8b43ccb59d5b65ea298641e275a844
krantamaki <kasper.rantamaki@csc.fi>
2023-05-05 11:37:21 +0300

    Improved error handling and allowing command line args

--- Poisson/python-scripts/plot_scalability.py
+++ Poisson/python-scripts/plot_scalability.py
@@ -1,10 +1,13 @@
 """
-Basic Python script that plots the execution times of a given linear solver as a function of the number of elements and fits an exponential to it
+Basic Python script that plots the execution times for given linear solvers as a function of the number of elements and fits an exponential to it
 """
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import os
+import sys
+import warnings
 from scipy.optimize import curve_fit
 
 
@@ -19,13 +22,17 @@ columns = ["Line Marker", "dofs", "elements", "partitions", "norm", "total CPU t
            "linsys real time (s)", "solver CPU time (s)", "solver real time (s)",
            "MeshLevel"]
 
-lin_method_id = 2
 use_time = "linsys CPU time (s)"  # Must be listed in columns
 
+colors = iter(mcolors.TABLEAU_COLORS.keys())
 
-def main():
-    data = pd.read_table(f"f{lin_method_id}.dat", delim_whitespace=True, header=None)
-    markers = pd.read_table(f"f{lin_method_id}.dat.marker", sep=':', header=None)
+# Initialize the figure in global scope
+plt.figure()
+
+
+def read_and_plot(dat_file):
+    data = pd.read_table(dat_file, delim_whitespace=True, header=None)
+    markers = pd.read_table(f"{dat_file}.marker", sep=':', header=None)
 
     solver = markers[1].values.tolist()
 
@@ -39,17 +46,37 @@ def main():
     elements = data['elements'].values.tolist()
 
     # Fit an exponential to the given datapoints
-    popt, pcov = curve_fit(lambda n, a, b: a * n ** b, elements, times)
-    x = np.linspace(np.min(elements), np.max(elements), 100)
-    y = popt[0] * x ** popt[1]
+    curve_found = False
+    try:
+        popt, pcov = curve_fit(lambda n, a, b: a * n ** b, elements, times)
+    except RuntimeError:
+        warnings.warn("Could not fit a curve to the datapoints. Plotting just the points.", RuntimeWarning)
+    else:
+        x = np.linspace(np.min(elements), np.max(elements), 100)
+        y = popt[0] * x ** popt[1]
+        curve_found = True
 
-    # Plot the datapoints and fitted exponential
-    plt.figure()
-    plt.loglog(x, y, 'k', label=f"Found exponential: $({popt[0]:.2e}) \cdot n ^ {{{popt[1]:.2e}}}$")
-    plt.loglog(elements, times, 'r.', label="Datapoints")
+    color = next(colors)
+
+    plt.loglog(elements, times, color=color, marker='o', linestyle='None', mfc='none', alpha=0.7,
+               label=f"Datapoints for solver: {data['Solver'][0]}")
+    if curve_found:
+        plt.loglog(x, y, color=color, alpha=0.5,
+                   label=f"Found exponential: $({popt[0]:.2e}) \cdot n ^ {{{popt[1]:.2e}}}$")
+
+
+def main():
+    if len(sys.argv) > 1:
+        dat_files = sys.argv[1:]
+    else:
+        # Predefined value. Will be overwritten by passed command line args
+        dat_files = ["f2.dat"]
+
+    for dat_file in dat_files:
+        read_and_plot(dat_file)
+        
     plt.ylabel(use_time)
     plt.xlabel("#Elements = $n$")
-    plt.title(f"Solver: {data['Solver'][0]}")  # Assumes that the file contains results for only one solver
     plt.legend()
     plt.show()
     
