78997c89b09fded70f92a2eca6703b7b8cf9f81f
Thomas Zwinger <zwinger@ninja-login.lnx.csc.fi>
2023-05-30 13:27:05 +0300

    finalized disagnostic run switching to Bueler profile

--- Stokes/eismint.lua
+++ Stokes/eismint.lua
@@ -13,11 +13,11 @@ n=3.0
 Ao=1.0E-16/spy
 a=0.3/spy
 L=750.0e03
-
+Tc=-5.0
 -- rate factors and molar activiation energies
 -- after Paterson 2010
-A1 = 2.89165e-13*spy*(pa_to_mpa^3) 
-A2 = 2.42736e-02*spy*(pa_to_mpa^3) 
+A1 = 2.89165e-13*spy*(pa_to_mpa^(-3.0)) 
+A2 = 2.42736e-02*spy*(pa_to_mpa^(-3.0)) 
 Q1 = 60.0e3
 Q2 = 115.0e3
 
@@ -31,4 +31,20 @@ function elev(x,y)
 	 end  
 	 s=( 4 * ( (1/2)^(4/3) - (r/2)^(4/3) ) )^ (3/8)
 	 return (Z-hmin)*s + hmin
-end	 
+end
+
+function bueler(x,y)
+ 	 r = math.sqrt(x*x + y*y)/L
+	 if (r > 1.0) then
+	   r = 1.0
+	 end 
+	 T1 =  Z/((n - 1.0)^(n/(2.0*n + 2.0)))
+	 T2 = (n + 1.0)*(r) - n*((r)^((n + 1.0)/n))
+	 T3 = n*((1.0 - (r))^((n + 1.0)/n) ) - 1.0
+	 H =  T1 * ( (T2 + T3)^(n/(2.0*n + 2.0)) )
+	 if (H > hmin) then
+	 	return H
+         else
+	        return hmin
+	 end
+end
