78815088211f841d5045bf265714fef1c96be42a
krantamaki <kasper.rantamaki@csc.fi>
2023-05-31 10:42:42 +0300

    Removing unnecessary files

--- Stokes/makegrid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gmsh -1 -2 circular.geo
-ElmerGrid 14 2 circular.msh -autoclean -scale 1000.0 1000.0 1.0 
-gmsh -1 -2 circular_coarse.geo
-ElmerGrid 14 2 circular_coarse.msh -autoclean -scale 1000.0 1000.0 1.0 
