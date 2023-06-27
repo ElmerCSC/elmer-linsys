192f6eefa4448cc32d5b20069ff60992eef815e4
krantamaki <kasper.rantamaki@csc.fi>
2023-06-27 09:57:55 +0300

    Mesh partitioning added to the script

--- run_complete.sh
+++ run_complete.sh
@@ -19,7 +19,7 @@ SCRIPT_PATH=python-scripts
 PROBLEM=Poisson
 
 # Define the number of partitions
-PARTITIONS=4
+PARTITIONS=3
 
 # Define the mesh levels to loop over
 MESH_LEVELS=(2 3 4)
@@ -39,16 +39,16 @@ TIME_PATH=$SCALE_PATH
 
 # USER CAN IF WANTED CHANGE FOLLOWING CONSTANTS:
 
-# Define the path where results are stored (no need to change)
+# Define the path where resulting .dat files are stored (no need to change)
 RET_PATH=$PWD/$CASE_PATH/results
 
 # Define the resulting .dat file (no need to change)
 RET_FILE=f$PARTITIONS.dat
 
-# Define the used tolerance
+# Define the used tolerance (no need to change)
 TOL=0.000001
 
-# Define if the total times should be plotted as well
+# Define if the total times should be plotted as well (no need to change)
 VIZ_TOT_TIME=false
 
 # Remove the result files if they already exist
@@ -57,13 +57,39 @@ VIZ_TOT_TIME=false
 # Copy the valid case file into the case.sif file
 # This can be commented out if there is only a single
 # default case file in the folder
-cp $CASE_PATH/case_all.sif $CASE_PATH/case.sif
+# cp $CASE_PATH/case_all.sif $CASE_PATH/case.sif
 
 
 ORG_DIR=$PWD
 
 
-# RUN THE SCRIPTS
+##################### RUN THE SCRIPTS #######################
+
+
+# CHECK FOR PARTITIONINGS AND AS REQUIRED PARTITION THE MESH TO NEEDED AMOUNT
+
+cd $CASE_PATH
+
+# Find all folders with files of form mesh.*
+MESH_DIRS=$(find . -type f -name "mesh.*" | sed -r 's|/[^/]+$||' |sort |uniq)
+
+for mesh_dir in "${MESH_DIRS[@]}"; do
+
+    # Check if proper partitioning already exists
+    if [ -f "$mesh_dir/partitioning.$PARTITIONS" ]; then
+	continue
+	
+    # Otherwise call ElmerGrid
+    else
+	ElmerGrid 2 2 $mesh_dir -partdual -metiskway $PARTITIONS
+    fi
+
+done
+
+cd $ORG_DIR
+
+
+# RUN THE ELMERSOLVER
 
 echo "Running ElmerSolver..."
 
@@ -106,6 +132,9 @@ for mesh_level in "${MESH_LEVELS[@]}"; do
     done
     
 done
+
+
+# VISUALIZE THE RESULTS
 
 cd $SCRIPT_PATH
 
