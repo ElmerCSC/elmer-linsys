7015acd35182915c482b4ddcaf6b4a3168059a1c
Thomas Zwinger <zwingerthomas@gmail.com>
2024-07-09 14:58:10 +0300

    updates

--- Poisson/WinkelUnstructured-ChEESE/run_ChEESE.sh
+++ Poisson/WinkelUnstructured-ChEESE/run_ChEESE.sh
@@ -12,16 +12,20 @@
 #SBATCH --job-name=ChEESE_test
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
-#SBATCH --partition=standard
+#SBATCH --partition=small
 
 ####### change to your project #######
-#SBATCH --account=project_462000233
+#SBATCH --account=project_462000007
 
 ####### change to numbers of nodes and MPI tasks ###
 ####### NB: we provide meshes for 128,256,512 and 1024 partitions #####
 #######     do the math by matching the product of next entries   #####
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+
+#SBATCH --exclusive                                                                                                                
+#SBATCH --mem=0 
 
 ################## OpenMP Stuff ##########
 ## use only if you undersubscribe
@@ -37,13 +41,15 @@ echo "running OpenMP on $SLURM_CPUS_PER_TASK"
 ml use /appl/local/csc/modulefiles
 # this loads the spack-PrgEnv-gnu cray-libsci (BLAS, LAPACK) version
 ###### best to use this for audits! #########
-ml use /appl/lumi/spack/23.03/0.19.2/share/spack/modules/linux-sles15-zen2
-ml use /appl/local/csc/soft/eng/elmer/spack/23.03/0.19.2/modules/tcl/linux-sles15-zen2/
-ml load elmer/gcc-spack
+#ml use /appl/lumi/spack/23.03/0.19.2/share/spack/modules/linux-sles15-zen2
+#ml use /appl/local/csc/soft/eng/elmer/spack/23.03/0.19.2/modules/tcl/linux-sles15-zen2/
+#ml load elmer/gcc-spack
+
 ###### this loads the container version of Elmer
 # module load elmer/latest
 ###### this loads the PregEnv/gnu version ###
 ###### (using cray-libsci (BLAS, LAPACK)
-#module load elmer/gcc-cray
-###### make it so! ######### 
-srun ElmerSolver case.sif
+module load elmer/gcc-cray
+###### make it so! #########
+ipar=2
+srun ElmerSolver case_fixedparams.sif
